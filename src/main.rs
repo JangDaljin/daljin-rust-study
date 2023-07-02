999007d8c956d55068911d4b25fbd949d12e7543
JangDaljin <toyyj15@naver.com>
2023-07-02 22:05:24 +0900

    stdin/file

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,55 @@
+use std::{
+    fs::File,
+    io::{self, BufRead, BufReader},
+};
 
-use ferris_says::{say};
-use std::io::{stdout, BufWriter};
+use clap::{App, Arg};
+use regex::Regex;
+
+fn process_lines<T: BufRead + Sized>(reader: T, re: Regex) {
+    for line_ in reader.lines() {
+        let line = line_.unwrap();
+        if line == "" {
+            return;
+        }
+        match re.find(&line) {
+            Some(_) => println!("{}", line),
+            None => (),
+        }
+    }
+}
 
 fn main() {
-    let stdout = stdout();
-    let message = String::from("Hello fellow Rustaceans!");
-    let width = message.chars().count();
+    let args = App::new("grep-lite")
+        .version("0.1")
+        .about("searches for patterns")
+        .arg(
+            Arg::with_name("pattern")
+                .help("1111")
+                .takes_value(true)
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("input")
+                .help("2222")
+                .takes_value(true)
+                .required(false),
+        )
+        .get_matches();
 
-    let mut writer = BufWriter::new(stdout.lock());
-    say(message.as_bytes(), width, &mut writer).unwrap();
+    let pattern = args.value_of("pattern").unwrap();
+    let re = Regex::new(pattern).unwrap();
 
+    let input = args.value_of("input").unwrap_or("-");
+
+    if input == "-" {
+        let stdin = io::stdin();
+        let reader = stdin.lock();
+        println!("asdfadf");
+        process_lines(reader, re);
+    } else {
+        let f = File::open(input).unwrap();
+        let reader = BufReader::new(f);
+        process_lines(reader, re);
+    }
 }
